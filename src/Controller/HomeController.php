e26146ee272260b43eeef22e0429bc7e423ffd27
Satar92 <moisatar@hotmail.com>
2023-06-20 17:01:29 +0200

    main

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use App\Service\AppHelpers;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     // #[Route('/', name: 'app_home')]
-    public function index(AppHelpers $app): Response
+    public function index(AppHelpers $app, ManagerRegistry $doctrine): Response
     {
         $app->installBDD();
-        return $this->render('home/index.html.twig');
+
+
+        $db = $doctrine->getManager();
+        $categories = $db->getRepository(Categorie::class)->findAll();
+        dd($categories);
+
+
+
+        return $this->render('home/index.html.twig',['cat' => $categories,]);
+            
     }
 }
